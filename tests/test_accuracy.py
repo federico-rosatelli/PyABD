6e9a02aee6fdcad010ed3cde86214179cefdd497
federico-rosatelli <frosatelli@gmail.com>
2026-03-17 10:51:42 +0100

    Update test for Breakfast dataset to include multiple K values in grid search

--- tests/test_accuracy.py
+++ tests/test_accuracy.py
@@ -5,7 +5,7 @@ from src.modes.offline_mode import run_grid_search
 def testBrakfast():
     alphas_to_test = [0.1, 0.2, 0.4, 0.6, 0.8]
 
-    ks_to_test = [7] 
+    ks_to_test = [5,7,6,10,12] 
 
     run_grid_search(
         dataset_name="breakfast", 
